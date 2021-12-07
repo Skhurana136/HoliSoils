00274b28e3375da5e214ef38e464aaad1fe88b42
Skhurana <62461235+Skhurana136@users.noreply.github.com>
2021-12-07 16:40:44 +0100

    Streamlined graphical outputs to plot shannon vs carbon

--- Scripts/graphical_outputs.py
+++ Scripts/graphical_outputs.py
@@ -3,8 +3,7 @@
 import os
 import pandas as pd
 import seaborn as sns
-
-from DS.solvers.diff_eqn_system import diversity_carbon
+import matplotlib.pyplot as plt
 
 ## LOAD RESULTS
 project_dir = "C:/Users/swami/Documents/Projects/HoliSoils/data"
@@ -13,6 +12,17 @@ figures_dir = os.path.join(project_dir, "figures")
 
 # Shannon diversity vs carbon stock
 data = pd.read_pickle(os.path.join(results_dir, "diversity_data.pkl"))
-
+data_norm = pd.read_pickle(os.path.join(results_dir, "diversity_normalized_data.pkl"))
 #%%
 sns.scatterplot(x="Shannon", y = "DOC", data = data)
+
+#%%
+sns.scatterplot(x="Shannon", y = "DOC", data = data_norm, hue = "carbon_species", size = "biomass_species")
+plt.ylabel("Normalised DOC")
+plt.yscale("log")
+
+#%%
+data_norm["S_DOC"] = data_norm["Shannon"]/data_norm["DOC"]
+sns.scatterplot(x="S_DOC", y = "DOC", data = data_norm, hue = "carbon_species", size = "biomass_species", edgecolor = None)
+plt.ylabel("Normalised DOC")
+plt.yscale("log")
