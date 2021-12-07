8cfd1a3689a21cadb2839bee4becaf086f135f13
Skhurana <62461235+Skhurana136@users.noreply.github.com>
2021-12-07 16:41:10 +0100

    Added functionality to evaluate shannon with normalized carbon concentrations

--- Scripts/simulations_processing.py
+++ Scripts/simulations_processing.py
@@ -4,26 +4,27 @@ import pandas as pd
 import numpy as np
 import h5py
 
-from DS.solvers.diff_eqn_system import diversity_carbon
+from DS.analyses.steady_state import diversity_carbon, normalize_carbon
 
 ## LOAD RESULTS
 project_dir = "C:/Users/swami/Documents/Projects/HoliSoils/data"
 results_dir = os.path.join(project_dir, "simulations")
 output_dir = os.path.join(project_dir, "results")
 
-hw = h5py.File(os.path.join(results_dir,"simulations.h5"), mode = 'r')
+hr = h5py.File(os.path.join(results_dir,"simulations.h5"), mode = 'r')
 
-# Load all datasets and save their Shannone and diversity indices in a dataframe:
+# Load all datasets and save their Shannon and diversity indices in a dataframe:
 
-n = len(hw)
+n = len(hr)
 
 emptydf = pd.DataFrame (columns = ["Sim", "carbon_species", "biomass_species", "Shannon", "DOC", "TOC"])
+emptydf_norm = pd.DataFrame (columns = ["Sim", "carbon_species", "biomass_species", "Shannon", "DOC", "TOC"])
 
 for sim in list(range(n)):
-    data = hw[str(sim)]
+    sim_data = hr[str(sim)]
     # Identify number of dom and microbial species:
-    dom_n, bio_n = data['species_number']
-    x = data['solution']
+    dom_n, bio_n = sim_data['species_number']
+    x = sim_data['solution']
     C = x['dom']
     B = x['biomass']
     data = np.append(C,B,axis=1)
@@ -31,12 +32,27 @@ for sim in list(range(n)):
     S_series = pd.Series(S)
     DOC_series = pd.Series(DOC)
     TOC_series =pd.Series(TOC)
-    sim_series = pd.Series(np.zeros(len(S)+sim))
-    dom_series = pd.Series(np.zeros(len(S)+dom_n))
-    bio_series = pd.Series(np.zeros(len(S)+bio_n))
+    sim_series = pd.Series(np.zeros(len(S))+sim)
+    dom_series = pd.Series(np.zeros(len(S))+dom_n)
+    bio_series = pd.Series(np.zeros(len(S))+bio_n)
     sim_df = pd.DataFrame ({"Sim":sim_series, "carbon_species":dom_series, "biomass_species":bio_series,
     "Shannon":S_series, "DOC":DOC_series, "TOC":TOC_series})
     emptydf = emptydf.append(sim_df, ignore_index=True)
+
+    initial_data = sim_data['initial_conditions']
+    carbon_initial = initial_data['dom']
+    biomass_initial = initial_data['biomass']
+
+    C_norm, B = normalize_carbon(data, dom_n, bio_n, carbon_initial, biomass_initial)
+    data_norm = np.append(C_norm[:,None],B,axis=1)
+    S, DOC, TOC = diversity_carbon(data_norm, dom_n, bio_n)
+    S_series = pd.Series(S)
+    DOC_series = pd.Series(DOC)
+    TOC_series =pd.Series(TOC)
+    sim_df = pd.DataFrame ({"Sim":sim_series, "carbon_species":dom_series, "biomass_species":bio_series,
+    "Shannon":S_series, "DOC":DOC_series, "TOC":TOC_series})
+    emptydf_norm = emptydf_norm.append(sim_df, ignore_index=True)
+
 
 print("The shape of the merged dataframe is ", emptydf.shape)
 print("The dataframe contains the following data types ", emptydf.dtypes)
@@ -44,3 +60,7 @@ print("The dataframe contains the following data types ", emptydf.dtypes)
 filename = os.path.join(output_dir, "diversity_data.pkl")
 emptydf.to_pickle(filename)
 print ("Diversity with carbon data is saved here ", filename)
+
+filename = os.path.join(output_dir, "diversity_normalized_data.pkl")
+emptydf_norm.to_pickle(filename)
+print ("Diversity with normalized carbon data is saved here ", filename)
