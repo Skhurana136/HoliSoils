3c61f467713a45fb0ef5ff6d7c7798e133c7a296
Swamini Khurana <swkh9804@win.su.se>
2022-03-01 17:14:14 +0100

    Updated scripts to incorporate fresh updates to the differential equation system solver

--- Scripts/test_network.py
+++ Scripts/test_network.py
@@ -3,6 +3,7 @@
 import numpy as np
 import os
 import matplotlib.pyplot as plt
+import matplotlib.lines as mlines
 
 from DS.solvers.diff_eqn_system import ReactionNetwork as rn
 from DS.analyses.steady_state import diversity_carbon, normalize_carbon
@@ -19,24 +20,24 @@ print ("Project directories set up.")
 #%%
 # Initialize the entire system
 # declare a time vector (time window)
-t_span = [0,100000]
+t_span = [0,100]
 t = np.arange(t_span[0], t_span [1],0.01)
 
 # Number of DOM/Carbon species:
 
-dom_n = np.random.randint(5,10,1)[0]
-bio_n = np.random.randint(4,10,1)[0]
+dom_n = np.random.randint(5,50,1)[0]
+bio_n = np.random.randint(4,50,1)[0]
 print(dom_n, bio_n)
 # Initialize the same number of parameters:
 
 enzparams = np.random.uniform(0.2, 0.4, bio_n)
-vparams = np.random.normal(0.8, 0.001, bio_n*dom_n)#np.random.uniform(0.001,15,bio_n*dom_n)
+vparams = np.random.normal(0.5, 0.001, bio_n*dom_n)#np.random.uniform(0.001,15,bio_n*dom_n)
 kparams = np.random.normal(600, 200, bio_n*dom_n)#np.random.randint(1000,8000,bio_n*dom_n)
 yparams = np.random.normal(0.5, 0.01, bio_n)#np.random.uniform(0.1,0.99,bio_n)
-mparams = np.mean(vparams.reshape(dom_n, bio_n),axis=0)/5
+mparams = np.min(vparams.reshape(dom_n, bio_n),axis=0)/5#15
 
 # initial conditions
-dom_initial = np.random.normal(400, 100, dom_n)#np.random.randint(500,1000,dom_n)
+dom_initial = np.random.normal(1000, 100, dom_n)#np.random.randint(500,1000,dom_n)
 biomass_initial = np.random.normal(400, 50, bio_n)#np.random.randint(100,1000,bio_n)
 x0 = np.append(dom_initial,biomass_initial)
 
@@ -60,17 +61,20 @@ trial.identify_components_natures()
 solution = trial.solve_network(x0, t_span, t)
 
 tim = solution.t
-sim_array = solution.y
-C1 = sim_array[trial.most_labile_c,:]
-C2 = sim_array[trial.least_labile_c,:]
-C = sim_array[:dom_n,:].T
-B = sim_array[dom_n:,:].T
-#plt.plot(tim, C1, linestyle = '-', label = "Simp_C")
-#plt.plot(tim, C2, linestyle = '-', label = "Comp_C")
+sim_array = solution.y.T
+C1 = sim_array[:,trial.most_labile_c,]
+C2 = sim_array[:,trial.least_labile_c]
+C = sim_array[:,:dom_n]
+B = sim_array[:,dom_n:]
 plt.figure()
 plt.plot (tim,B, linestyle = '--')#, label = "Bacteria")
 plt.plot (tim,C, linestyle = '-')#, label = "Bacteria")
-#plt.ylim(bottom=0.0)
+plt.xlabel ("Time (days)")
+plt.ylabel ("Concentration")
+plt.legend()
+solid_line = mlines.Line2D([], [], linestyle = '-', color='grey', linewidth=1.5, label='DOM')
+dashed_line = mlines.Line2D([], [], linestyle = '--', color='grey', linewidth=1.5, label='Biomass')
+first_legend = plt.legend(handles=[solid_line, dashed_line])
 
 
 #plt.figure()
@@ -80,10 +84,7 @@ plt.plot (tim,C, linestyle = '-')#, label = "Bacteria")
 
 #%%# Shannon diversity
 S, DOC, TOC = diversity_carbon(sim_array, dom_n, bio_n)
-
-#%%
-plt.scatter(S, TOC)
-#plt.yscale("log")
+plt.scatter(x = S, y = DOC)
 
 #%%
 plt.hist(yparams, bins = 20)
