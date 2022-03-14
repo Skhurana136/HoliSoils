30f5cc3088e03508c70a52886069fbe660add924
Swamini Khurana <swkh9804@win.su.se>
2022-03-14 16:27:59 +0100

    Updated notebook to account for updated module in diff_eqn_system

--- Scripts/test_network.py
+++ Scripts/test_network.py
@@ -23,7 +23,7 @@ project_dir = "C:/Users/swkh9804/Documents/Projects/HoliSoils/data"
 project_dir = "C:/Users/swami/Documents/Projects/HoliSoils/data"
 #%%
 # Assign child directories:
-details_subfolder = 'varied_c_07032022'
+details_subfolder = 'varied_c_14032022_changed_lin_mort_v4'
 simulations_dir = os.path.join(project_dir, "simulations", details_subfolder)
 results_dir = os.path.join(project_dir, "results", details_subfolder)
 figures_dir = os.path.join(project_dir, "figures", details_subfolder)
@@ -45,21 +45,33 @@ t_span = [0,500]
 t = np.arange(t_span[0], t_span [1],0.01)
 
 #%%
-# Number of DOM/Carbon species:
 empty_dic = {}
 for i in list(range(4)):
     np.random.seed(i)
     dom_n = np.random.randint(5,20,1)[0]
     bio_n = np.random.randint(4,20,1)[0]
     print(dom_n, bio_n)
-    enzparams, vparams, kparams, yparams, mparams = generate_random_parameters(dom_n, bio_n,5)
-    dom_initial, biomass_initial = generate_random_initial_conditions (dom_n, bio_n)
+    ox_state, enzparams, zparams, vparams, kparams, yparams, mparams = generate_random_parameters(dom_n, bio_n,5)
+    dom_initial, biomass_initial = generate_random_initial_conditions (dom_n, bio_n, 1000, 80)
     x0 = np.append(dom_initial, biomass_initial)
 
-    carbon_input = generate_random_boundary_conditions(dom_n, 50, method_name = "user_defined")
+    carbon_input = generate_random_boundary_conditions(dom_n, 100, method_name = "user_defined")
+    
+    trial = rn(maximum_capacity=5,
+            #carbon_mol_bio = 10,
+            carbon_num = dom_n,
+            bio_num = bio_n,
+            carbon_input = carbon_input,
+            sigmoid_coeff_stolpovsky = 0.01,
+            necromass_distribution="notequal")
+    trial.set_rate_constants(ox_state, enzparams, zparams, vparams, kparams, yparams,mparams)
+    trial.identify_components_natures(recalcitrance_criterion="oxidation_state")
+    solution = trial.solve_network(x0, t_span, t)
 
     seed_dic = {i : {'dom_number': dom_n, 'biomass_number': bio_n,
+    'oxidation_state': ox_state,
     'enzyme_production_parameters': enzparams,
+    'uptake_parameters': zparams,
     'max_rate_parameters': vparams,
     'sat_const_parameters': kparams,
     'efficiency_parameters': yparams,
@@ -67,22 +79,8 @@ for i in list(range(4)):
     'initial_conditions_dom': dom_initial,
     'initial_conditions_biomass': biomass_initial,
     'carbon_input_boundary': carbon_input}}
-    
     empty_dic.update(seed_dic)
-
-    trial = rn(maximum_capacity=5,
-            #carbon_mol_bio = 10,
-            carbon_num = dom_n,
-            bio_num = bio_n,
-            carbon_input = carbon_input,
-            sigmoid_coeff_stolpovsky = 0.01,
-            enzyme_production_rate_constant = enzparams[0],
-            efficiency_bio_uptake = enzparams[1],
-            necromass_distribution="notequal")
-    trial.set_rate_constants(vparams, kparams, yparams,mparams)
-    trial.identify_components_natures()
-    solution = trial.solve_network(x0, t_span, t)
-
+    
     tim = solution.t
     sim_array = solution.y.T
 
@@ -107,13 +105,14 @@ for i in list(range(4)):
     #total_C_stock = np.sum(C,axis=1) + np.sum(B, axis=1)
     #C_stock = np.sum(C,axis=1)
 
-    plt.figure()
+    fig = plt.figure()
     plt.plot (tim,B, linestyle = '--')#, label = "Bacteria")
     plt.plot (tim,C, linestyle = '-')#, label = "Bacteria")
     plt.xlabel ("Time (days)")
     plt.ylabel ("Concentration")
     plt.legend()
-    plt.text(s = "Seed: "+str(i), x = 80, y = 800, fontsize = 12)
+    plt.text(s = "Seed: "+str(i), x = 0.8, y = 0.8,
+    fontsize = 12)
     solid_line = mlines.Line2D([], [], linestyle = '-', color='grey', linewidth=1.5, label='DOM')
     dashed_line = mlines.Line2D([], [], linestyle = '--', color='grey', linewidth=1.5, label='Biomass')
     first_legend = plt.legend(handles=[solid_line, dashed_line])
@@ -157,3 +156,24 @@ l_j = np.asarray([0.01, 0.02, 0.01]).reshape(-1,1)
 zakem_q_i_j = np.divide(rho_i_j, l_j)*np.sum(np.multiply(y_i_j,rho_i_j)/rho_i_j) + y_i_j
 #%%
 labile_c = np.flipud(np.argsort(np.max(zakem_q_i_j, axis=0)))
+#%%
+dom_n = 3#np.random.randint(5,20,1)[0]
+bio_n = 3#np.random.randint(4,20,1)[0]
+print(dom_n, bio_n)
+ox_state, enzparams, zparams, vparams, kparams, yparams, mparams = generate_random_parameters(dom_n, bio_n,5)
+labile_c = np.argsort(ox_state)
+most_labile_c = labile_c[0]
+least_labile_c = labile_c[-1]
+middle_labile_group = labile_c[1:]
+#%%
+zparams = zparams.reshape(dom_n, bio_n)
+vparams = vparams.reshape(dom_n, bio_n)
+kparams = kparams.reshape(dom_n, bio_n)
+yparams = yparams.reshape(dom_n, bio_n)
+#%%
+print ("Most recalcitrant carbon pool is : ", least_labile_c)
+print ("Most labile carbon pool is : ", most_labile_c)
+z = np.sort(zparams)[:,::-1][:,labile_c] #More labile compounds will be taken up more efficiently from the environment
+vparams = np.sort(vparams)[:,::-1][:,labile_c] #More labile compounds will have faster max rate of consumption
+kparams = np.sort(kparams)[:,labile_c] #More labile compounds will have lower saturation constant
+yparams = np.sort(yparams)[:,::-1][:,labile_c] #Microbes will grow more efficiently on more labile compounds 
