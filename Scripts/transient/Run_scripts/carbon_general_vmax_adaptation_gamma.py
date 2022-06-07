4c237f5bb5303e4893c7be625e54db7f523afbdf
Skhurana136 <swamini.khurana@gmail.com>
2022-06-07 11:37:24 +0200

    Updated script for gamma adapt

--- Scripts/transient/Run_scripts/carbon_general_vmax_adaptation_gamma.py
+++ Scripts/transient/Run_scripts/carbon_general_vmax_adaptation_gamma.py
@@ -14,9 +14,9 @@ from DS.solvers.diff_eqn_system import generate_random_boundary_conditions
 
 project_dir = os.path.join("D:/", "Projects", "HoliSoils","data","transient")
 
-seed_sim_list = [610229235, 983307757, 643338060, 714504443]#, 277077803, 898393994, 420,13012022,13061989]
+seed_sim_list = [610229235, 983307757, 643338060, 714504443, 277077803, 898393994, 420,13012022,13061989]
 
-cn_list = [12,18]#[3,6]#,12,18]
+cn_list = [3,6, 12,18]
 bio_n_series = [4,8,12,16,20,24,28,32]
 ip = 0 #0 random scenarios
 init_dom_list = [1000,2000,5000,10000,15000]
