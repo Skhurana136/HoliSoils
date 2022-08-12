abe048f5d50bb5028d57fbb8e8f8304a44b4eb6f
Skhurana136 <swamini.khurana@gmail.com>
2022-08-12 10:50:59 +0200

    Updated job submit script

--- Scripts/Linux/transient/Run_scripts/activity_loss_-02/jobs.sh
+++ Scripts/Linux/transient/Run_scripts/activity_loss_-02/jobs.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 #
-#SBATCH -J test_carbon_null
-#SBATCH -t 00:60:00
+#SBATCH --job-name==test_carbon_null
+#SBATCH --time=00:60:00
 #SBATCH --mem=2000
 #SBATCH -n 1
+#SBATCH --chdir=/proj/hs_micro_div_072022
+#SBATCH --output=./Reports/output_%j.out
+#SBATCH --error=./Reports/error_%j.err
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=swamini.khurana@natgeo.su.se
 #
@@ -13,5 +16,4 @@ module load foss/2020b
 module load Anaconda/2021.05-nsc1
 conda activate ds-envsci-env
 python "/home/x_swakh/tools/HoliSoils/Scripts/Linux/transient/Run_scripts/activity_loss_-02/carbon_switch_off_null.py" "null"
-#
 # Scripts ends here
