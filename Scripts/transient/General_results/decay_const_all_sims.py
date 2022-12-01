06ca827f76bb70b430713456cb60d26eb532d3f4
Skhurana136 <swamini.khurana@gmail.com>
2022-12-01 14:46:25 +0100

    temporal_decay_const_data_analysis

--- Scripts/transient/General_results/decay_const_all_sims.py
+++ Scripts/transient/General_results/decay_const_all_sims.py
@@ -1,3 +1,4 @@
+#%%
 import os
 import pandas as pd
 import numpy as np
@@ -6,7 +7,7 @@ import numpy as np
 project_dir = os.path.join("D:/", "Projects", "HoliSoils","data","transient")
 sim_suffixes = ["_0_01", "_0_1", "_0_5", "", "_1_5x"]
 sim_suffixes_var = [0.01, 0.1, 0.5, 1, 1.5]
-
+#%%
 files = []
 for p,a in zip(sim_suffixes, sim_suffixes_var):
     filename = os.path.join(project_dir, "gen_spec_lognorm" + p, "results", "competition_adaptation_carbon__loss_0.9_cue_combined_dataset.pkl")
@@ -18,6 +19,7 @@ for p,a in zip(sim_suffixes, sim_suffixes_var):
 fd_data = pd.concat(files)
 print(fd_data.columns)
 fd_data['DOC_initial_int'] = round(fd_data.DOC_initial, -3)
+#%%
 ### CHARACTERISTIC REACTION TIME SCALE
 files = []
 for p,a in zip(sim_suffixes, sim_suffixes_var):
@@ -30,7 +32,8 @@ for p,a in zip(sim_suffixes, sim_suffixes_var):
 tim_data = pd.concat(files)
 print(tim_data.columns)
 tim_data['DOC_initial_int'] = round(tim_data.DOC_initial, -3)
-cols_to_merge = ['Seed', 'Sim_series', 'carbon_species', 'biomass_species', 'T_10', 'T_10_base', 'T20', 'T20_base', 'T50', 'T50_base', 'activity', 'Variance',  'DOC_initial_int']
+#%%
+cols_to_merge = ['Seed', 'Sim_series', 'carbon_species', 'biomass_species', 'C_pool', 'T_10', 'T10_base', 'T_20', 'T20_base', 'T_50', 'T50_base', 'activity', 'Variance',  'DOC_initial_int']
 fd_tim_data = pd.merge(fd_data, tim_data[cols_to_merge], on = ["Seed", "Variance", "DOC_initial_int","biomass_species", "carbon_species", "Sim_series", "activity"])
 files = []
 for p,a in zip(sim_suffixes, sim_suffixes_var):
@@ -45,14 +48,13 @@ print(para_data.columns)
 cols_to_merge = para_data.columns.to_list()[2:]
 all_data = pd.merge(fd_tim_data, para_data[cols_to_merge], on = ["Seed", "Variance", "biomass_species", "carbon_species", "Sim_series"])
 all_data["active_H_c_connections"] = all_data['S_initial']*all_data['carbon_species']*all_data["activity"]/100
-all_data["Decay_constant_10"] = 1/(all_data['T10']*5).to_numpy(dtype = float)    
-all_data["Decay_constant_20"] = 1/(all_data['T20']*5).to_numpy(dtype = float)    
-all_data["Decay_constant_50"] = 1/(all_data['T50']*5).to_numpy(dtype = float)
+all_data["Decay_constant_10"] = 1/(all_data['T_10']*5).to_numpy(dtype = float)    
+all_data["Decay_constant_20"] = 1/(all_data['T_20']*5).to_numpy(dtype = float)    
+all_data["Decay_constant_50"] = 1/(all_data['T_50']*5).to_numpy(dtype = float)
 all_data["active_H"] = all_data['S_initial']*all_data["activity"]/100
 all_data["FD_cov"]= np.sqrt(all_data.FD_initial)/all_data.vmax_mean
 init_doc_list = np.sort(list(all_data.DOC_initial_int.unique()))
 activity_list = np.sort(list(all_data.activity.unique()))
-s_initial_list = np.sort(list(all_data.S_initial_int.unique()))
 seed_sim_list = np.sort(list(all_data.Seed.unique()))
 c_sp_list = list(all_data.carbon_species.unique())
 bio_sp_list = list(all_data.biomass_species.unique())
