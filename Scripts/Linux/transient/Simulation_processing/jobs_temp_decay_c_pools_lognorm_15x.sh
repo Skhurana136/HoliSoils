2d170b3375d3ff7cf4b243c6c3a9941dd3089d75
Skhurana136 <swamini.khurana@gmail.com>
2022-12-07 11:18:42 +0100

    Evaluate temporal variation in decay constant using initial conditions as baseline

--- Scripts/Linux/transient/Simulation_processing/jobs_temp_decay_c_pools_lognorm_15x.sh
+++ Scripts/Linux/transient/Simulation_processing/jobs_temp_decay_c_pools_lognorm_15x.sh
@@ -15,5 +15,6 @@ module load buildtool-easybuild/4.5.3-nsce8837e7
 module load foss/2020b
 module load Anaconda/2021.05-nsc1
 conda activate ds-envsci-env
-python "/home/x_swakh/tools/HoliSoils/Scripts/Linux/transient/Simulation_processing/decay_const_diff_c_pools_time.py" "gen_spec_lognorm_1_5x"
+#python "/home/x_swakh/tools/HoliSoils/Scripts/Linux/transient/Simulation_processing/decay_const_diff_c_pools_time.py" "gen_spec_lognorm_1_5x"
+python "/home/x_swakh/tools/HoliSoils/Scripts/Linux/transient/Simulation_processing/decay_const_diff_c_pools_time_initial_conditions.py" "gen_spec_lognorm_1_5x"
 # Scripts ends here
