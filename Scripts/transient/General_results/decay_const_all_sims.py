7226631c8753d392baaf5debd1d75dfdb889ace3
Skhurana136 <swamini.khurana@gmail.com>
2022-12-13 15:47:22 +0100

    data processing

--- Scripts/transient/General_results/decay_const_all_sims.py
+++ Scripts/transient/General_results/decay_const_all_sims.py
@@ -10,10 +10,13 @@ sim_suffixes_var = [0.01, 0.1, 0.5, 1, 1.5]
 
 files = []
 for p,a in zip(sim_suffixes, sim_suffixes_var):
-    filename = os.path.join(project_dir, "gen_spec_lognorm" + p, "results", "competition_adaptation_carbon__loss_0.9_cue_combined_dataset.pkl")
+    filename = os.path.join(project_dir, "gen_spec_lognorm" + p, "results", "competition_adaptation_carbon_cue_wremaining_c_data.pkl")
     data = pd.read_pickle(filename)
     var_arr = np.zeros((data.shape[0],))+a
     var_ser = pd.Series(var_arr, copy=False,name = "Variance")
+    cases = list(data.Sim_series.unique())
+    for c, a in zip (cases, [100, 10, 10, 10, 10, 10, 25, 25, 25, 25, 25, 50, 50, 50, 50, 50, 75, 75, 75, 75, 75]):        
+        data.loc[data['Sim_series']==c, 'activity'] = a
     data_var = data.join(var_ser)
     files.append(data_var)
 fd_data = pd.concat(files)
@@ -23,10 +26,13 @@ fd_data['DOC_initial_int'] = round(fd_data.DOC_initial, -3)
 ### CHARACTERISTIC REACTION TIME SCALE
 files = []
 for p,a in zip(sim_suffixes, sim_suffixes_var):
-    filename = os.path.join(project_dir, "gen_spec_lognorm" + p, "results", "competition_adaptation_carbon_loss_temporal_temporal_decay_const_c_pools_initial_conditions_combined_dataset.pkl")
+    filename = os.path.join(project_dir, "gen_spec_lognorm" + p, "results", "competition_adaptation_carbon__decay_const_c_pools_data_initial_conditions.pkl")
     data = pd.read_pickle(filename)
     var_arr = np.zeros((data.shape[0],))+a
     var_ser = pd.Series(var_arr, copy=False,name = "Variance")
+    cases = list(data.Sim_series.unique())
+    for c, a in zip (cases, [100, 10, 10, 10, 10, 10, 25, 25, 25, 25, 25, 50, 50, 50, 50, 50, 75, 75, 75, 75, 75]):        
+        data.loc[data['Sim_series']==c, 'activity'] = a
     data_var = data.join(var_ser)
     files.append(data_var)
 tim_data = pd.concat(files)
@@ -45,41 +51,41 @@ para_data = pd.concat(files)
 print(para_data.columns)
 cols_to_merge = para_data.columns.to_list()[2:]
 all_data = pd.merge(fd_tim_data, para_data[cols_to_merge], on = ["Seed", "Variance", "biomass_species", "carbon_species", "Sim_series"])
-all_data["active_H_c_connections"] = all_data['S_initial']*all_data['carbon_species']*all_data["activity"]/100
-all_data["active_H"] = all_data['S_initial']*all_data["activity"]/100
-all_data["FD_cov"]= np.sqrt(all_data.FD_initial)/all_data.vmax_mean
+#all_data["active_H_c_connections"] = all_data['S_initial']*all_data['carbon_species']*all_data["activity"]/100
+#all_data["active_H"] = all_data['S_initial']*all_data["activity"]/100
+#all_data["FD_cov"]= np.sqrt(all_data.FD_initial)/all_data.vmax_mean
 init_doc_list = np.sort(list(all_data.DOC_initial_int.unique()))
 activity_list = np.sort(list(all_data.activity.unique()))
 seed_sim_list = np.sort(list(all_data.Seed.unique()))
 c_sp_list = list(all_data.carbon_species.unique())
 bio_sp_list = list(all_data.biomass_species.unique())
-T_cols = list(x for x in all_data.columns if 'T' in x)
-old_t = 'T0'
-for t_col in T_cols:
-    if t_col == 'T0':
-        all_data['dec0'] = 1/(all_data['T0']*5)
-    else:
-        #all_data['dec'+t_col[1:]] = 1/(all_data[t_col]*5) #execute when all calcs are for subsequent calcs
-        all_data['T_diff'+t_col[1:]] = abs((all_data[t_col]-all_data[old_t])) #execute when all calcs are wrt initial conditions
-        all_data['dec'+t_col[1:]] = 1/(all_data['T_diff'+t_col[1:]]*5) #execute when all calcs are wrt initial conditions
-    all_data['dec_ratio'+t_col[1:]]=all_data['dec'+t_col[1:]]/all_data['dec0']#compl = all_data.dropna(subset = ['ratio_t_50'])
-    old_t = t_col
+#T_cols = list(x for x in all_data.columns if 'T' in x)
+#old_t = 'T0'
+#for t_col in T_cols:
+#    if t_col == 'T0':
+#        all_data['dec0'] = 1/(all_data['T0']*5)
+#    else:
+#        #all_data['dec'+t_col[1:]] = 1/(all_data[t_col]*5) #execute when all calcs are for subsequent calcs
+#        all_data['T_diff'+t_col[1:]] = abs((all_data[t_col]-all_data[old_t])) #execute when all calcs are wrt initial conditions
+#        all_data['dec'+t_col[1:]] = 1/(all_data['T_diff'+t_col[1:]]*5) #execute when all calcs are wrt initial conditions
+#    all_data['dec_ratio'+t_col[1:]]=all_data['dec'+t_col[1:]]/all_data['dec0']#compl = all_data.dropna(subset = ['ratio_t_50'])
+#   old_t = t_col
 
-for v in [0.01, 0.1, 0.5, 1., 1.5]:
-    for s in seed_sim_list:
-        for c in c_sp_list:
-            for b in bio_sp_list:
-                for d in init_doc_list:
-                    sub_scb = all_data[(all_data["Variance"]==v)&(all_data["Seed"].astype(int)==s)&(all_data["carbon_species"].astype(int)==c)&(all_data["biomass_species"].astype(int)==b)&(all_data["DOC_initial_int"].astype(int)==d)]
-                    fd_base = sub_scb[sub_scb.Sim_series=="b_1_all_"]['FD_initial'].values[0]
-                    fd_cov_base = sub_scb[sub_scb.Sim_series=="b_1_all_"]['FD_cov'].values[0]
-                    v_mean_base = sub_scb[sub_scb.Sim_series.isin(['b_1_all_'])]['vmax_mean'].mean()
-                    all_data.loc[(all_data["Variance"]==v)&(all_data["Seed"].astype(int)==s)&(all_data["carbon_species"].astype(int)==c)&(all_data["biomass_species"].astype(int)==b)&(all_data["DOC_initial_int"].astype(int)==d), "vmax_mean_base"]=v_mean_base
-                    all_data.loc[(all_data["Variance"]==v)&(all_data["Seed"].astype(int)==s)&(all_data["carbon_species"].astype(int)==c)&(all_data["biomass_species"].astype(int)==b)&(all_data["DOC_initial_int"].astype(int)==d), "FD_initial_base"]=fd_base
-                    all_data.loc[(all_data["Variance"]==v)&(all_data["Seed"].astype(int)==s)&(all_data["carbon_species"].astype(int)==c)&(all_data["biomass_species"].astype(int)==b)&(all_data["DOC_initial_int"].astype(int)==d), "FD_cov_base"]=fd_cov_base
-all_data['vmax_ratio'] = all_data.vmax_mean/all_data.vmax_mean_base
-all_data['FD_ratio'] = all_data.FD_maxbio/all_data.FD_initial
-all_data['Biomass_ratio'] = all_data.Biomass_maxbio/all_data.Biomass_initial
-all_data["FD_initial_ratio"] = all_data.FD_initial/all_data.FD_initial_base                    
-all_data["FD_cov_ratio"] = all_data.FD_cov/all_data.FD_cov_base
+#for v in [0.01, 0.1, 0.5, 1., 1.5]:
+#    for s in seed_sim_list:
+#        for c in c_sp_list:
+#            for b in bio_sp_list:
+#                for d in init_doc_list:
+#                    sub_scb = all_data[(all_data["Variance"]==v)&(all_data["Seed"].astype(int)==s)&(all_data["carbon_species"].astype(int)==c)&(all_data["biomass_species"].astype(int)==b)&(all_data["DOC_initial_int"].astype(int)==d)]
+#                    fd_base = sub_scb[sub_scb.Sim_series=="b_1_all_"]['FD_initial'].values[0]
+#                    fd_cov_base = sub_scb[sub_scb.Sim_series=="b_1_all_"]['FD_cov'].values[0]
+#                    v_mean_base = sub_scb[sub_scb.Sim_series.isin(['b_1_all_'])]['vmax_mean'].mean()
+#                    all_data.loc[(all_data["Variance"]==v)&(all_data["Seed"].astype(int)==s)&(all_data["carbon_species"].astype(int)==c)&(all_data["biomass_species"].astype(int)==b)&(all_data["DOC_initial_int"].astype(int)==d), "vmax_mean_base"]=v_mean_base
+#                    all_data.loc[(all_data["Variance"]==v)&(all_data["Seed"].astype(int)==s)&(all_data["carbon_species"].astype(int)==c)&(all_data["biomass_species"].astype(int)==b)&(all_data["DOC_initial_int"].astype(int)==d), "FD_initial_base"]=fd_base
+#                    all_data.loc[(all_data["Variance"]==v)&(all_data["Seed"].astype(int)==s)&(all_data["carbon_species"].astype(int)==c)&(all_data["biomass_species"].astype(int)==b)&(all_data["DOC_initial_int"].astype(int)==d), "FD_cov_base"]=fd_cov_base
+#all_data['vmax_ratio'] = all_data.vmax_mean/all_data.vmax_mean_base
+#all_data['FD_ratio'] = all_data.FD_maxbio/all_data.FD_initial
+#all_data['Biomass_ratio'] = all_data.Biomass_maxbio/all_data.Biomass_initial
+#all_data["FD_initial_ratio"] = all_data.FD_initial/all_data.FD_initial_base                    
+#all_data["FD_cov_ratio"] = all_data.FD_cov/all_data.FD_cov_base
 all_data.to_csv(os.path.join(project_dir,"simulation_results_temporal_initial_conditions_decay_const.csv"),index=False)
--- Scripts/transient/Simulation_processing/Combine_datasets_decay_const_c_pools.py
+++ Scripts/transient/Simulation_processing/Combine_datasets_decay_const_c_pools.py
@@ -3,36 +3,46 @@ import os
 import pandas as pd
 import numpy as np
 import sys
+import itertools
 
 ## LOAD RESULTS
 project_dir = os.path.join("D:/", "Projects", "HoliSoils","data","transient", sys.argv[1])
 results_dir = os.path.join(project_dir, "results")
 filestring = "competition_adaptation_carbon_"
-results_filename = filestring + "loss_temporal_temporal_decay_const_c_pools_initial_conditions_combined_dataset"
-
-seed_sim_list = [610229235, 983307757, 643338060, 714504443, 277077803, 898393994, 420,13012022,13061989]
-cn_list = [3,6,12,18]
-
+results_filename = filestring + "_decay_const_c_pools_data_initial_conditions_combined_dataset"
+filename = os.path.join(results_dir, filestring + "_decay_const_c_pools_data_initial_conditions.pkl")
+all_data = pd.read_pickle(filename)
+all_data['DOC_initial']=all_data["DOC_initial"].astype(int)    
+seed_sim_list = all_data.Seed.unique().tolist()#[610229235, 983307757, 643338060, 714504443, 277077803, 898393994, 420,13012022,13061989]
+cn_list = all_data.carbon_species.unique().tolist()
+bios = all_data.biomass_species.unique().tolist()
+carb_types = ['DOC','reduced_C','necromass','oxidized_C']
+docs = all_data.DOC_initial.unique().tolist()
+sims = all_data.Sim_series.unique().tolist()
+all_data.rename(columns={'DOC':'Tdoc', 'reduced_C':'Tredc', 'oxidized_C':'Toxc'}, inplace=True)
 count = 0
-for c_n in cn_list:
-    row = []
-    filename = os.path.join(results_dir, filestring + str(c_n) + "_loss_temporal_temporal_decay_const_c_pools_data_initial_conditions.pkl")
-    diversity_data = pd.read_pickle(filename)
-    # Additional data processing
-    if count == 0:
-        combined_data = diversity_data
-    else:
-        combined_data = pd.concat([combined_data, diversity_data]).reindex()
-    count +=1
+for c, b, d, seed, s in itertools.product(cn_list,bios, docs, seed_sim_list, sims):
+    subset = all_data[(all_data["Seed"].astype(int)==seed)&(all_data["carbon_species"].astype(int)==c)&(all_data["biomass_species"].astype(int)==b)&(all_data["DOC_initial"].astype(int)==d)&(all_data["Sim_series"]==s)]
+    print(c,b,d,seed,s)
+    print(subset[['%C','Tdoc']])
+    #print(c_n, b_n, doci, seed, sim)
+#for c_n in cn_list:
+#    row = []
+#    # Additional data processing
+#    if count == 0:
+#        combined_data = diversity_data
+#    else:
+#        combined_data = pd.concat([combined_data, diversity_data]).reindex()
+#    count +=1
 
-print(combined_data.shape)
-combined_data.drop_duplicates()
-print(combined_data.shape)
+#print(combined_data.shape)
+#combined_data.drop_duplicates()
+#print(combined_data.shape)
 
-cases = list(diversity_data.Sim_series.unique())
-for c, a in zip (cases, [100, 10, 10, 10, 10, 10, 25, 25, 25, 25, 25, 50, 50, 50, 50, 50, 75, 75, 75, 75, 75]):        
-    combined_data.loc[combined_data['Sim_series']==c, 'activity'] = a
+#cases = list(diversity_data.Sim_series.unique())
+#for c, a in zip (cases, [100, 10, 10, 10, 10, 10, 25, 25, 25, 25, 25, 50, 50, 50, 50, 50, 75, 75, 75, 75, 75]):        
+#    combined_data.loc[combined_data['Sim_series']==c, 'activity'] = a
 
-combined_data = combined_data.drop_duplicates()
-combined_data.to_csv(os.path.join(project_dir, "results", results_filename+".csv"))
-combined_data.to_pickle(os.path.join(project_dir, "results", results_filename + ".pkl"))
+#combined_data = combined_data.drop_duplicates()
+#combined_data.to_csv(os.path.join(project_dir, "results", results_filename+".csv"))
+#combined_data.to_pickle(os.path.join(project_dir, "results", results_filename + ".pkl"))
