14021d611954d80faf859b1d59d4bbd873d3c99c
Skhurana136 <swamini.khurana@gmail.com>
2022-12-14 11:15:49 +0100

    Combine all assembled data in one dataframe

--- Scripts/transient/General_results/decay_const_all_sims.py
+++ Scripts/transient/General_results/decay_const_all_sims.py
@@ -10,22 +10,27 @@ results_dir = os.path.join(project_dir, "results")
 sim_suffixes = ["_0_01", "_0_1", "_0_5", "", "_1_5x"]
 sim_suffixes_var = [0.01, 0.1, 0.5, 1, 1.5]
 
-fd_file = os.path.join("results","competition_adaptation_carbon_cue_wremaining_c_data_combined_dataset.pkl")
-tim_file = os.path.join("results","competition_adaptation_carbon__decay_const_c_pools_data_initial_conditions_combined_dataset.pkl")
-para_file = os.path.join("results","competition_adaptation_parameters_combined_dataset.pkl")
+fd_file = os.path.join(results_dir,"competition_adaptation_carbon_cue_wremaining_c_data_combined_dataset.pkl")
+tim_file = os.path.join(results_dir,"competition_adaptation_carbon__decay_const_c_pools_data_initial_conditions_combined_dataset.pkl")
+para_file = os.path.join(results_dir,"competition_adaptation_parameters_combined_dataset.pkl")
 fd_data = pd.read_pickle(fd_file)
 tim_data = pd.read_pickle(tim_file)
 para_data = pd.read_pickle(para_file)
 
-fd_tim_data = pd.merge(fd_data, tim_data, on = ["Seed", "Variance", "DOC_initial_int","biomass_species", "carbon_species", "Sim_series", "activity"])
+#fd_tim_data = pd.merge(fd_data, tim_data, on = ["Seed", "Variance", "DOC_initial_int","biomass_species", "carbon_species", "Sim_series", "activity"])
 
-cols_to_merge = para_data.columns.to_list()[2:]
-all_data = pd.merge(fd_tim_data, para_data[cols_to_merge], on = ["Seed", "Variance", "biomass_species", "carbon_species", "Sim_series"])
-all_data["active_H_c_connections"] = all_data['S_initial']*all_data['carbon_species']*all_data["activity"]/100
-all_data["active_H"] = all_data['S_initial']*all_data["activity"]/100
+#cols_to_merge = para_data.columns.to_list()[2:]
+fd_tim_data = pd.merge(fd_data,tim_data, on = ["Seed", "Variance", "DOC_initial_int","biomass_species", "carbon_species", "Sim_series", "activity"], suffixes=('', '_y'))
+
+fd_tim_data.drop(fd_tim_data.filter(regex='_y$').columns, axis=1, inplace=True)
+all_data = pd.merge(fd_tim_data, para_data, on = ["Seed", "Variance", "biomass_species", "carbon_species", "Sim_series"],suffixes=('', '_y'))
+all_data.drop(all_data.filter(regex='_y$').columns, axis=1, inplace=True)
+print(all_data.columns)
+#all_data["active_H_c_connections"] = all_data['S_initial']*all_data['carbon_species']*all_data["activity"]/100
+#all_data["active_H"] = all_data['S_initial']*all_data["activity"]/100
 
 init_doc_list = np.sort(list(all_data.DOC_initial_int.unique()))
-activity_list = np.sort(list(all_data.activity.unique()))
+#activity_list = np.sort(list(all_data.activity_x.unique()))
 seed_sim_list = np.sort(list(all_data.Seed.unique()))
 c_sp_list = list(all_data.carbon_species.unique())
 bio_sp_list = list(all_data.biomass_species.unique())
