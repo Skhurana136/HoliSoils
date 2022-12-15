34710aaedc0cdfc053cbb926d7e3121bcd862eef
Skhurana136 <swamini.khurana@gmail.com>
2022-12-15 23:17:40 +0100

    Removed for loop and tried a pandas substitute

--- Scripts/Linux/transient/Simulation_processing/Combine_all_datasets.py
+++ Scripts/Linux/transient/Simulation_processing/Combine_all_datasets.py
@@ -38,10 +38,17 @@ c_sp_list = list(all_data.carbon_species.unique())
 bio_sp_list = list(all_data.biomass_species.unique())
 sim_list = list(all_data.Sim_series.unique())
 
-for v,s,c,b,d,sim in itertools.product([0.01, 0.1, 0.5, 1., 1.5],seed_sim_list, c_sp_list,bio_sp_list,init_doc_list,sim_list):
-    sub_scb = all_data[(all_data["Variance"]==v)&(all_data["Seed"].astype(int)==s)&(all_data["carbon_species"].astype(int)==c)&(all_data["biomass_species"].astype(int)==b)&(all_data["DOC_initial_int"].astype(int)==d)&(all_data["Sim_series"]==sim)]
-    decay_initial = sub_scb[sub_scb['%C']==100.]['decay_const'].values[0]
-    all_data.loc[(all_data["Variance"]==v)&(all_data["Seed"].astype(int)==s)&(all_data["carbon_species"].astype(int)==c)&(all_data["biomass_species"].astype(int)==b)&(all_data["DOC_initial_int"].astype(int)==d)&(all_data["Sim_series"]==sim), "Decay_const_initial"]=decay_initial
+all_data['Decay_const'] = 1/all_data['T_loss']
+initial_data = all_data[all_data['%C']==100.]
+initial_data.rename(columns={'Decay_const':'Decay_const_initial'}, inplace=True)
+tim_initial_data = pd.merge(all_data, initial_data, on = ["Seed", "Variance", "biomass_species", "carbon_species", "Sim_series", "C_pool"],suffixes=('', '_y'))
+tim_initial_data.drop(tim_initial_data.filter(regex='_y$').columns, axis=1, inplace=True)
+#tim_initial_data.to_csv(os.path.join("C:\Users\swkh9804\Documents\Project_data\HoliSoilssimulation_results_temporal_initial_conditions_decay_const_modified.csv"), index=False)
+
+#for v,s,c,b,d,sim in itertools.product([0.01, 0.1, 0.5, 1., 1.5],seed_sim_list, c_sp_list,bio_sp_list,init_doc_list,sim_list):
+#    sub_scb = all_data[(all_data["Variance"]==v)&(all_data["Seed"].astype(int)==s)&(all_data["carbon_species"].astype(int)==c)&(all_data["biomass_species"].astype(int)==b)&(all_data["DOC_initial_int"].astype(int)==d)&(all_data["Sim_series"]==sim)]
+#    decay_initial = sub_scb[sub_scb['%C']==100.]['decay_const'].values[0]
+#    all_data.loc[(all_data["Variance"]==v)&(all_data["Seed"].astype(int)==s)&(all_data["carbon_species"].astype(int)==c)&(all_data["biomass_species"].astype(int)==b)&(all_data["DOC_initial_int"].astype(int)==d)&(all_data["Sim_series"]==sim), "Decay_const_initial"]=decay_initial
 #all_data["FD_cov"]= np.sqrt(all_data.FD_initial)/all_data.vmax_mean
 #all_data['vmax_ratio'] = all_data.vmax_mean/all_data.vmax_mean_base
 all_data['decay_ratio'] = all_data.decay_const/all_data.Decay_const_initial
