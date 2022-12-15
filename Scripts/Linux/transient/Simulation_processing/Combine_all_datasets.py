c2fba108761f1ce4cc6efd11638d1eb9599525f1
Skhurana136 <swamini.khurana@gmail.com>
2022-12-15 16:19:45 +0100

    Correction

--- Scripts/Linux/transient/Simulation_processing/Combine_all_datasets.py
+++ Scripts/Linux/transient/Simulation_processing/Combine_all_datasets.py
@@ -26,7 +26,7 @@ fd_tim_data.drop(fd_tim_data.filter(regex='_y$').columns, axis=1, inplace=True)
 all_data = pd.merge(fd_tim_data, para_data, on = ["Seed", "Variance", "biomass_species", "carbon_species", "Sim_series"],suffixes=('', '_y'))
 all_data.drop(all_data.filter(regex='_y$').columns, axis=1, inplace=True)
 all_data['FD_ratio'] = all_data.FD/all_data.FD_initial
-all_data['Biomass_ratio'] = all_data.Biomass_maxbio/all_data.Biomass_initial
+all_data['Biomass_ratio'] = all_data.Biomass/all_data.Biomass_initial
 all_data["active_H_c_connections"] = all_data['S_initial']*all_data['carbon_species']*all_data["activity"]/100
 all_data["active_H"] = all_data['S_initial']*all_data["activity"]/100
 print(all_data.columns)
